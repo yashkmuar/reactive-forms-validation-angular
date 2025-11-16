c99590e04c24787e3c9e5d4597ee707d0e861974
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-16 13:46:25 +0530

    grouping of forms is done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('forms-grouping-angular');
+
+  profileForm = new FormGroup({
+    name:new FormControl('yash'),
+    password:new FormControl('455'),
+    email:new FormControl('pindwar@email.com')
+  })
+
+  onSubmit(){
+    console.log(this.profileForm.value);
+  }
+
+  setValue(){
+    this.profileForm.setValue({
+      name:'Kyla',
+      password:'russia',
+      email:'leconti@tjx.com'
+    })
+  }
 }
